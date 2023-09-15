bd17dec6d06766d6288587e4c150fd001d563296
Linesgreen <linesgreen@mail.ru>
2023-09-16 02:03:00 +0300

    SPPPPPPPPPPPPAT'

---
+++ Two Highes.js
@@ -0,0 +1,31 @@
+/* In this kata, your job is to return the two distinct highest values in a list. 
+If there're less than 2 unique values, return as many of them, as possible.
+
+The result should also be ordered from highest to lowest.
+
+Examples:
+
+[4, 10, 10, 9]  =>  [10, 9]
+[1, 1, 1]  =>  [1]
+[]  =>  [] */
+
+
+function twoHighest(arr) {
+   let x = Math.max(...arr) ; // Впервые применяю поиск максимального значения в массиве
+   let y = 0 ;
+   switch (arr.length) {
+    case 0: return [];
+    case 1: return [arr[0]];
+   }
+  for (i = 0; i<arr.length; i++) 
+ {
+if ((y < arr[i]) && x != arr[i]) {
+    y = arr[i];
+    console.log('y=' + y)
+}
+ }
+ return [x,y]; }
+
+
+
+  console.log(twoHighest([15,11,2,11]));
