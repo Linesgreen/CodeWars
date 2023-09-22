085acabd7bf21274d76e92fd8e5b8d63ac7aa8b4
Linesgreen <linesgreen@mail.ru>
2023-09-22 19:52:46 +0300

    non repeat leter

---
+++ 7kyu/Non Repeated Character.js
@@ -0,0 +1,34 @@
+/* You need to write a function, that returns the first non-repeated character in the given string.
+
+If all the characters are unique, return the first character of the string.
+If there is no unique character, return null in JS or Java, None in Python, '\0' in C.
+You can assume, that the input string has always non-zero length.
+
+Examples
+"test"   returns "e"
+"teeter" returns "r"
+"trend"  returns "t" (all the characters are unique)
+"aabbcc" returns null (all the characters are repeated) */
+
+function firstNonRepeated(s) {
+    let  testWord = s;
+    let count = 0;
+    for (i = 0; i < s.length; i += 1)
+    {
+        var regular = new RegExp(s[i]); 
+        
+        testWord = s.replace(s[i], '*');
+       
+        if (regular.test(testWord) == false ) {
+            return s[i];
+        } else { count += 1; }
+        
+        
+    }
+    if ( count = s.length) {return null}
+    return s[0];
+  }
+
+
+
+  console.log(firstNonRepeated('vvlladd'));
