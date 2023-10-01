3dc292a61fd57d278e951ae25cb682f158b4ca1e
Vladislav Evg <linesgreen@yandex.by>
2023-10-01 12:47:12 +0300

    1.0

---
+++ 7kyu/bils.js
@@ -0,0 +1,14 @@
+function splitTheBill(x) {
+    let sum = 0; 
+    // Первый цикл для нахождения общей суммы счета
+    for (key in x) {
+        sum += x[key] ;    
+    } 
+    sum = sum / Object.keys(x).length
+// Второй цикл для определения задолженности
+    for (key in x) {
+        x[key] = (x[key] - sum);    
+        Number.isInteger(x[key]) == true ? null: x[key] = Number(x[key].toFixed(2));
+    } 
+    return x;
+}
