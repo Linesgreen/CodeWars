4a78905b10cb185f122c4a51070823f00a5364c9
Vladislav Evg <linesgreen@yandex.by>
2023-10-02 17:42:43 +0300

    переделать

---
+++ 6kyu/Who like thise.js
@@ -0,0 +1,39 @@
+/* You probably know the "like" system from Facebook and other pages.
+ People can "like" blog posts, pictures or other items. 
+ We want to create the text that should be displayed next to such an item.
+
+Implement the function which takes an array containing the names of people
+ that like an item. It must return the display text as shown in the examples: 
+
+[]                                -->  "no one likes this"
+["Peter"]                         -->  "Peter likes this"
+["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+
+Note: For 4 or more names, the number in "and 2 others" simply increases.*/
+
+
+
+function likes(names) {
+    let answer = "";
+    let firstName = names[0];
+    let secondName = names[1];
+    let thridName = names[2];
+    if (names.length == 0 ) {
+                return answer = "no one likes this";
+            }  else if(names.length == 1) {
+                answer = firstName  + " likes this"
+            } 
+            else if (names.length == 2) {
+                answer = firstName + ' and ' + secondName  + " like this"
+            } else if(names.length == 3) {
+                answer = firstName + ', ' + secondName  + " and " + thridName + " like this"}
+               else { answer = firstName + ', ' + secondName  + " and " + String(names.length - 2) + " others like this" }
+            return answer
+        }
+
+
+  
+
+console.log(likes(['Peter']));
