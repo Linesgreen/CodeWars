0f772b8dd2b071ca4eb22b792dab978286456c1a
Vladislav Evg <linesgreen@mail.ru>
2023-10-16 12:26:47 +0300

    0.1

---
+++ 6kyu/replaceAlphabetPosition.js
@@ -0,0 +1,7 @@
+function alphabetPosition(text) {
+  let Arrletters = text.toLowerCase().match(/[a-z]/g)
+   return  Arrletters === null ? -1 : Arrletters.map(letter => (letter.charCodeAt(0)) - 96).join(" ") ;
+  }
+
+  
+  console.log(alphabetPosition("#"));
